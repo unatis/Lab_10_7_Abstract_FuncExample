8ddd3a466aed6f106b0c738bb6b4f911ec5db1e4
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-02 15:24:38 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //FirstOne firstOne = new FirstOne();//error - no object creation
+
+        SecondOne secondOne = new SecondOne();
+
+        secondOne.SomeFunction();
+
+    }
+}
